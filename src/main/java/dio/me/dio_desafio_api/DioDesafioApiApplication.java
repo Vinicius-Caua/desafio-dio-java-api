5858b97b82681ddbaba4f599861f6455a3e165c1
Vinicius Caua <vinipg1@hotmail.com>
2024-07-23 15:55:12 -0300

    Initial commit

---
+++ src/main/java/dio/me/dio_desafio_api/DioDesafioApiApplication.java
@@ -0,0 +1,13 @@
+package dio.me.dio_desafio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/dio_desafio_api/DioDesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.dio_desafio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
